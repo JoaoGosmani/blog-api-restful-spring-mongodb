bd4d448ef4c87513d3df56eaad00c04eb96147c2
Joao Gosmani <joaogosmani@gmail.com>
2022-12-21 16:05:27 -0300

    Projeto criado

---
+++ src/main/java/br/com/joaogosmani/blogapirestful/BlogApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.blogapirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiRestfulApplication.class, args);
+	}
+
+}
